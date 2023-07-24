bb149e32dac85fbda6a1756a0d22e2698fadf118
AbdulSameer0 <abdulsameer9685@gmail.com>
2023-07-25 00:33:58 +0530

    added my ui project with the help og spring boot

---
+++ student.management.system/src/main/java/com/sms/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.sms.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sms.entity.Student;
+
+
+
+public interface StudentRepository extends CrudRepository<Student, Long>{
+
+}
